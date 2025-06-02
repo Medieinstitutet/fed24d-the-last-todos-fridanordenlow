ba8ed851a33faa56a6f5a3a6fbe034319fad3644
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-06-02 15:27:16 +0200

    fix: update id constructor in Todo class to generate unique ID's

--- src/models/Todo.ts
+++ src/models/Todo.ts
@@ -5,7 +5,7 @@ export class Todo {
   createdAt: string = '';
 
   constructor(description: string) {
-    this.id = Date.now();
+    this.id = Date.now() + Math.floor(Math.random() * 1000);
     this.description = description;
     this.createdAt = new Date().toISOString();
   }
