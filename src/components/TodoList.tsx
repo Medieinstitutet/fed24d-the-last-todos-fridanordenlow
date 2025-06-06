ba8e2db1a24a6ecdd14a45c0c2629f0c5cb36974
Frida Nordenlöw <frida.nordenlow@hotmail.com>
2025-06-06 10:54:30 +0200

    chore: clean up code

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,27 +1,3 @@
-// import { Todo } from '../models/Todo';
-// import { TodoItem } from './TodoItem';
-
-// type TodoListProps = {
-//   todoList: Todo[];
-//   onDelete: (id: number) => void;
-//   onToggle: (id: number) => void;
-// };
-
-// export const TodoList = ({ todoList, onDelete, onToggle }: TodoListProps) => {
-//   return (
-//     <>
-//       {todoList.map((todo) => (
-//         <TodoItem
-//           key={todo.id}
-//           todoItem={todo}
-//           onDelete={onDelete}
-//           onToggle={onToggle}
-//         />
-//       ))}
-//     </>
-//   );
-// };
-
 import { Todo } from '../models/Todo';
 import { TodoItem } from './TodoItem';
 import { Box, Typography } from '@mui/material';
@@ -39,11 +15,8 @@ export const TodoList = ({ todoList, onDelete, onToggle }: TodoListProps) => {
         display: 'flex',
         flexDirection: 'column',
         gap: 2,
-        // p: 3,
         mt: 4,
         mb: 4,
-        // maxWidth: 500,
-        // margin: '0 auto',
       }}
     >
       {todoList.length === 0 ? (
